11064fc1f923abe7f97323ff4ba7bb2723422cae
Crete <140468648+Creteeee@users.noreply.github.com>
2025-07-16 20:51:42 +0800

    增加高亮功能

---
+++ BlueStar/Assets/Settings/OutlineObjectsManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineObjectsManager : MonoBehaviour
+{
+   
+
+    public List<GameObject> outlineObjects = new List<GameObject>();
+    public static List<GameObject> objsForRender = new List<GameObject>();
+    public Camera camera;
+    private GameObject previousObj;
+
+    private void Awake()
+    {
+        objsForRender.Clear();
+        //camera = Camera.main;
+    }
+
+    void Update()
+    {
+        objsForRender=outlineObjects;
+        if (camera != null)
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+
+            if (Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("InteractiveMesh","Click")))
+            {
+                if (hit.transform.gameObject != previousObj)
+                {
+                    objsForRender.Clear();
+                    objsForRender.Add(hit.collider.gameObject);
+                    previousObj=hit.transform.gameObject;
+                }
+
+            }
+
+            else
+            {
+                objsForRender.Clear();
+                previousObj = null;
+            }
+        }
+    }
+    
+}
---
+++ BlueStar/Assets/Settings/OutlineRenderPassFeature.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class OutlineRenderPassFeature : ScriptableRendererFeature
+{
+    [System.Serializable]
+    public class Settings
+    {
+        public Material outlineMaterial;
+        // 不能在这里引用场景对象
+    }
+
+    [SerializeField]
+    public Settings settings = new Settings(); // ← ← ← 这个必须加 SerializeField！
+
+    OutlineRenderPass m_ScriptablePass;
+
+    public override void Create()
+    {
+        m_ScriptablePass = new OutlineRenderPass(settings);
+        m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderer.EnqueuePass(m_ScriptablePass);
+    }
+
+    class OutlineRenderPass : ScriptableRenderPass
+    {
+        Settings settings;
+
+        public OutlineRenderPass(Settings settings)
+        {
+            this.settings = settings;
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            if (OutlineObjectsManager.objsForRender == null || OutlineObjectsManager.objsForRender.Count == 0)
+                return;
+            var cmd = CommandBufferPool.Get("Draw Outline");
+            Debug.unityLogger.Log( OutlineObjectsManager.objsForRender[0].name);
+
+            foreach (var obj in OutlineObjectsManager.objsForRender)
+            {
+                var renderer = obj?.GetComponent<Renderer>();
+                if (renderer != null)
+                {
+                    cmd.DrawRenderer(renderer, settings.outlineMaterial);
+                }
+            }
+
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+    }
+}
+
+
+
