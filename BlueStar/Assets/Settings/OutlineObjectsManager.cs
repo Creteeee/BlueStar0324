c02d16f30c2230b59f79e47d99eb0188be56bc14
Crete <140468648+Creteeee@users.noreply.github.com>
2025-07-21 08:23:01 +0800

    更新

--- BlueStar/Assets/Settings/OutlineObjectsManager.cs
+++ BlueStar/Assets/Settings/OutlineObjectsManager.cs
@@ -30,7 +30,7 @@ public class OutlineObjectsManager : MonoBehaviour
             if (Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("InteractiveMesh","Click")))
             {
                 Debug.Log(this.gameObject.name);
-                if (hit.transform.gameObject != previousObj)
+                if (hit.transform.gameObject != previousObj && ObserveItem.currentObserveObj== null)
                 {
                     objsForRender.Clear();
                     objsForRender.Add(hit.collider.gameObject);
